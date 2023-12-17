5e94fcf61bb14b9a55d4d24ad94db889fa7f3b0b
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2023-12-17 12:32:42 +0530

    flask server ready

---
+++ api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from trail import getPlant
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', methods=["GET"])
+def home():
+    response = {"message": "This is VedaVision Chatbot"}
+    return jsonify(response)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    query = request.get_json()
+    preprocessed_image = query.get("imgArray")
+    try:
+        result = getPlant(preprocessed_image)
+    except:
+        result = []
+    return jsonify({"idxs": result})
+
+if __name__ == "__main__":
+    app.run()
---
+++ trail.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import numpy as np
+
+# Load your pre-trained TensorFlow model
+model_path = './vedavision_densenetv3.h5'
+model = tf.keras.models.load_model(model_path)
+
+# Define the image preprocessing function
+
+
+
+def getPlant(preprocessed_image):
+    predictions = model.predict(preprocessed_image)
+
+    file_path = "./PlantsNameSeq1.txt"
+
+    # Open the file in read mode
+    plant_list = []
+    with open(file_path, 'r') as file:
+        # Read and print each line in the file
+        for line in file:
+            # print(line.strip())
+            # strip() removes leading and trailing whitespace characters
+            plant_list.append(line.strip())
+
+    # class_names = sorted(os.listdir(directory_path))
+    # Print the predicted class label
+    plant_list.sort()
+
+    max_index = np.argmax(predictions.flatten())
+
+    # Get the maximum value
+    max_value = predictions.flatten()
+
+    sorted_flatten = sorted(max_value, reverse=True)
+
+    indieces = []
+    max_value = max_value.tolist()
+    # print(type(max_value))
+    indieces.append(max_value.index(sorted_flatten[0]))
+    indieces.append(max_value.index(sorted_flatten[1]))
+    indieces.append(max_value.index(sorted_flatten[2]))
+
+    for index in indieces:
+        print(plant_list[index])
+    
+    return indieces
